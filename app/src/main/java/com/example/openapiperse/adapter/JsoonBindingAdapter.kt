89946b2dbfdd9345349dd65302ced137c74d4a55
earbaj <earbajsaria3@gmail.com>
2022-10-05 17:09:19 +0600

    add final polish to adding drawble image to show loading indicator image and broken image

--- app/src/main/java/com/example/openapiperse/adapter/JsoonBindingAdapter.kt
+++ app/src/main/java/com/example/openapiperse/adapter/JsoonBindingAdapter.kt
@@ -5,6 +5,7 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.openapiperse.R
 import com.example.openapiperse.data.JsonData
 
 @BindingAdapter("imageUrl")
@@ -12,7 +13,10 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
         // Load the image in the background using Coil.
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        imgView.load(imgUri)
+        imgView.load(imgUri){
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
     }
 }
 
